bfc45d4ae4477bd2d7b9937e3acb88a3c4a193b8
kuchaaar <kuba_kucharek@wp.pl>
2025-04-21 16:17:36 +0200

    config

--- config.py
+++ config.py
@@ -1,6 +1,18 @@
 import os
-EMAIL_SENDER = os.getenv("smtp-email")
-EMAIL_PASSWORD = os.getenv("smtp-password")
-SMTP_SERVER = os.getenv("smtp-server")
-SMTP_PORT = os.getenv("smtp-port")
+import os
+
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
+
+
+keyVaultName = os.getenv("key_vault_name")
 FRONTEND_URL = os.getenv("frontend-uri")
+KVUri = f"https://{keyVaultName}.vault.azure.net"
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=KVUri, credential=credential)
+
+EMAIL_SENDER = client.get_secret("smtp-email")
+EMAIL_PASSWORD = client.get_secret("smtp-password")
+SMTP_SERVER = client.get_secret("smtp-server")
+SMTP_PORT = client.get_secret("smtp-port")
