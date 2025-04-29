3dbb3218cf500965387b136160d7a0643dd4d1ff
Jakub Kucharek <131975376+Kuchaaar@users.noreply.github.com>
2025-04-29 17:37:53 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
-import os
-
 EMAIL_SENDER = os.getenv("smtp-email")
 EMAIL_PASSWORD = os.getenv("smtp-password")
 SMTP_SERVER = os.getenv("smtp-server")
 SMTP_PORT = os.getenv("smtp-port")
+FRONTEND_URL = os.getenv("frontend-uri")
